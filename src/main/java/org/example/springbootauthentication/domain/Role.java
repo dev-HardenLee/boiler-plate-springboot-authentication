d21a54e656309117c08a06c5c78dda06d63604e3
harden <harden.lee@companywe.co.kr>
2024-02-20 15:43:00 +0900

    Role 순환참조 엔티티로 변경

--- src/main/java/org/example/springbootauthentication/domain/Role.java
+++ src/main/java/org/example/springbootauthentication/domain/Role.java
@@ -1,10 +1,13 @@
 package org.example.springbootauthentication.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,8 +15,23 @@ import lombok.NoArgsConstructor;
 public class Role {
 
     @Id
+    @Column(name = "ROLE")
     private String role;
 
     private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PARENT_ROLE")
+    private Role parentRole;
+
+    @OneToMany(mappedBy = "parentRole")
+    private List<Role> childRole = new ArrayList<>();
+
+    @Builder
+    public Role(String role, String text, Role parentRole) {
+        this.role = role;
+        this.text = text;
+        this.parentRole = parentRole;
+    }
 
 }
